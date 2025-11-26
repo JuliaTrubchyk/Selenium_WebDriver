90f4873a7f619ad83336809bd09d7de8816fc02e
Yuliia <julia.koshovets@gmail.com>
2025-11-26 00:00:05 -0800

    Added geckodriver for FF browser. Added method to read the title from url on FF browser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -9,10 +10,22 @@ public class Main {
     public static void main(String[] args) {
         String chromeTitle = chromeTest("https://www.google.com/");
         System.out.println(chromeTitle);
+
+        String firefoxTitle = firefoxTest("https://www.facebook.com/");
+        System.out.println(firefoxTitle);
     }
 
     private static String chromeTest(String url){
         WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        String title = driver.getTitle();
+        driver.quit();
+        return title;
+    }
+
+    private static String firefoxTest(String url){
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        WebDriver driver = new FirefoxDriver();
         driver.get(url);
         String title = driver.getTitle();
         driver.quit();
