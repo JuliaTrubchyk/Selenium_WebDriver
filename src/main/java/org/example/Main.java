e7238401bd75bc352a31f720b765ff18a8f1e60b
Yuliia <julia.koshovets@gmail.com>
2025-11-29 14:07:24 -0800

    Practicing using Simple Locators

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,36 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String chromeTitle = chromeTest("https://www.google.com/");
-        System.out.println(chromeTitle);
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        // WebElement locators - id, name, className
+        WebElement usernameInputField = driver.findElement(By.id("username"));
+        WebElement passwordInputField = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.className("btn"));
 
-        String firefoxTitle = firefoxTest("https://www.facebook.com/");
-        System.out.println(firefoxTitle);
+        // Finding all input fields on the page
+        List<WebElement> inputFields = driver.findElements(By.tagName("input"));
+
+        // // WebElement locators - linkText, partialLinkText
+        WebElement linkTextLocator = driver.findElement(By.linkText("Practice Test Automation."));
+        WebElement partialLinkTextLocator = driver.findElement(By.partialLinkText("Test Automation"));
+
+        // Practicing Relative Locators
+        WebElement passwordFieldBelowUserName = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
+        WebElement privacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.linkText("Practice Test Automation.")));
+        driver.quit();
     }
 
     private static String chromeTest(String url){
