2bfebb5041f9ddbeaecc11e9e76ed6f90f4634a4
Yuliia <julia.koshovets@gmail.com>
2025-12-01 10:20:54 -0800

    Moved code from Main to separate method and created a new method findingExceptionPageElements

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,29 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+        findingExceptionPageElements(driver);
+        driver.quit();
+    }
+
+    private static String chromeTest(String url){
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        String title = driver.getTitle();
+        driver.quit();
+        return title;
+    }
+
+    private static String firefoxTest(String url){
+        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get(url);
+        String title = driver.getTitle();
+        driver.quit();
+        return title;
+    }
+
+    // Intro to using Selectors
+    private static void findingPageElements(WebDriver driver){
         driver.get("https://practicetestautomation.com/practice-test-login/");
         // WebElement locators - id, name, className
         WebElement usernameInputField = driver.findElement(By.id("username"));
@@ -40,24 +63,28 @@ public class Main {
         WebElement usernameInputFieldCss = driver.findElement(By.cssSelector("input[id='username']"));
         WebElement passwordInputFieldCss = driver.findElement(By.cssSelector("input[name='password']"));
         WebElement submitBtnCss = driver.findElement(By.cssSelector("button[id='submit']"));
-
-        driver.quit();
     }
 
-    private static String chromeTest(String url){
-        WebDriver driver = new ChromeDriver();
-        driver.get(url);
-        String title = driver.getTitle();
-        driver.quit();
-        return title;
-    }
+    private static void findingExceptionPageElements(WebDriver driver){
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+        WebElement linkTxt = driver.findElement(By.linkText("Selenium WebDriver with Java for beginners program"));
+        WebElement partialLinkTxt = driver.findElement(By.partialLinkText("Selenium WebDriver"));
 
-    private static String firefoxTest(String url){
-        System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-        WebDriver driver = new FirefoxDriver();
-        driver.get(url);
-        String title = driver.getTitle();
-        driver.quit();
-        return title;
+        WebElement inputFieldByTag = driver.findElement(By.tagName("input"));
+        WebElement inputFieldByClassName = driver.findElement(By.className("input-field"));
+        WebElement inputFieldByXpath = driver.findElement(By.xpath("//input[@value='Pizza']"));
+        WebElement inputFieldByCSS = driver.findElement(By.cssSelector("input[type='text']"));
+
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+
+        WebElement editBtnById = driver.findElement(By.id("edit_btn"));
+        WebElement editBtnByName = driver.findElement(By.name("Edit"));
+        WebElement editBtnByCss = driver.findElement(By.cssSelector("button[id='edit_btn']"));
+        WebElement editBtnByXpath = driver.findElement(By.xpath("//button[@name='Edit']"));
+
+        WebElement addBtnById = driver.findElement(By.id("add_btn"));
+        WebElement addBtnByName = driver.findElement(By.name("Add"));
+        WebElement addBtnByCss = driver.findElement(By.cssSelector("button[name='Add']"));
+        WebElement addBtnByXpath = driver.findElement(By.xpath("//button[@id='add_btn']"));
     }
 }
